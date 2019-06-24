8062de4b8742b7f96a167b0e0fbf9a834410e9be
sairamaj <sairamaj@gmail.com>
2019-06-23 18:32:06 -0700

    loop testing

---
+++ loops/fordowhile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main () {
+
+   int i = 1;
+
+   do {
+      printf("value of a: %d\n", i);
+      i++;
+   }while( i <= 10 );
+ 
+}
---
+++ loops/formain.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+   int i = 0;
+   
+    for(;i<=10;i++){
+        printf("Number is %d\n", i);
+    }
+}
